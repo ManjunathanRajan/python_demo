4bfb7ff472296743f3e6a71c24fdb967dfe668e5
Manjunathan Rajan <32861102+ManjunathanRajan@users.noreply.github.com>
2022-07-31 13:04:35 +0530

    Update ScriptToSendMail.py
    Updated receiver's email address

--- ScriptToSendMail.py
+++ ScriptToSendMail.py
@@ -3,7 +3,7 @@
 import smtplib
 
 fadd = 'manjublr92@gmail.com'                                      # sender's email address
-tadd = 'manjun.adhavan@vmscout.in'                                 # receiver's email address
+tadd = 'manju.adhavan@vmscout.in'                                  # receiver's email address
 msg = 'Mail sent through Python using Jenkins job!'                # Message to be sent!
 username = 'manjublr92@gmail.com'                                  # Your username(email ID)
 password = 'mynldzefwjerhnlv'                                      # Your password for above email ID
